7919b41f78d728ffe1ffa478944a0c45cf91010a
vruddhi2710 <59500931+vruddhi2710@users.noreply.github.com>
2021-05-26 23:54:28 +0530

    Add files via upload

---
+++ Task1/TimerExample/TimerExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+
+namespace TimerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);
+
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            myTimer.Start();
+            Console.WriteLine("press enter to remove the red event");
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss:fff}", e.SignalTime);
+
+            // throw new NotImplementedException();
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss:fff}",e.SignalTime);
+
+           // throw new NotImplementedException();
+        }
+    }
+}
