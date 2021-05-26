ea3d8488d07a1cd8a41ab52fe098364c22b9492a
vruddhi2710 <59500931+vruddhi2710@users.noreply.github.com>
2021-05-26 23:32:43 +0530

    Add files via upload

---
+++ Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.Write("Message -> ");
+            ReverseString("Message");
+            Console.WriteLine();
+            Display();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        private static void ReverseString(string msg)
+        {
+            char[] arr = msg.ToCharArray();
+            Array.Reverse(arr);
+            foreach (char item in arr)
+            {
+                Console.Write(item);
+            }
+        }
+
+        private static void Display()
+        {
+            string x = "hello";
+            string y = "Lucifer";
+            string z = "Morningstar";
+            Console.WriteLine(String.Format("{0}, {1} {2}.", x, y, z));
+        }
+    }
+}
