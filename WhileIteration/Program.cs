2e5e78ea5383459325483cdffcc860213eb4fd39
vruddhi2710 <59500931+vruddhi2710@users.noreply.github.com>
2021-05-26 23:39:18 +0530

    Add files via upload

---
+++ WhileIteration/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool displayMenu = true;
+            while (displayMenu)
+                displayMenu = MainMenu();
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1) Print Numbers!");
+            Console.WriteLine("2) Guessing Game!");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if (result == "1")
+                PrintNumbers();
+            else if (result == "2")
+                GuessingGame();
+            else if (result == "3")
+                return false;
+            return true;
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.Clear();
+            Console.WriteLine("Print Numbers!");
+            Console.Write("Enter a number: ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while(counter <= result)
+            {
+                Console.Write(counter + "-");
+                counter++;
+            }
+            Console.ReadLine();
+        }
+        private static void GuessingGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Guessing Game!");
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10: ");
+                string result = Console.ReadLine();
+                guesses++;
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+            } while (incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses!", guesses);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WorkingWithCollections/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "OldMobile";
+            car1.Model = "99";
+            car1.VIN = "A1";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Model = "Prism";
+            car2.VIN = "A2";
+
+            Book book = new Book();
+            book.Author = "Robert Downey";
+            book.Title = "Ironman : The Unsunged Hero";
+            book.ISBN = "0-000-00000-0";
+
+            /* ArrayList myAraayList = new ArrayList();
+             myAraayList.Add(car1);
+             myAraayList.Add(car2);
+             myAraayList.Add(book);
+             myAraayList.Remove(book);
+
+             foreach (Car car in myAraayList)
+             {
+                 Console.WriteLine(car.Make);
+             }*/
+
+            //List<T>
+            /*List<Car> myList = new List<Car>();
+            myList.Add(car1);
+            myList.Add(car2);
+            //myList.Add(book); //It  wont let you add car to the List.
+
+            foreach (Car car in myList)
+            {
+                Console.WriteLine(car.Model);
+            }*/
+
+            //Dictionary<Tkey, TValue>
+            /* Dictionary<string, Car> myDict = new Dictionary<string, Car>();
+             myDict.Add(car1.VIN, car1);
+             myDict.Add(car2.VIN, car2);
+
+             Console.WriteLine(myDict["A2"].Make);*/
+
+            //string[] names = { "Bob", "Charlie", "Brian", "Cook" };
+
+            //Object Initializer 
+            Car car3 = new Car() { Make = "BMW", Model = "97", VIN = "A3" };
+            Car car4 = new Car() { Make = "Mercedes", Model = "92", VIN = "A4" };
+
+            //Collection Initializer
+            List<Car> myList = new List<Car>()
+            {
+                new Car { Make = "Hyundai", Model = "95", VIN = "A5"},
+                new Car { Make = "Nissan", Model = "96", VIN = "A6"}
+            };
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string VIN { get; set; } 
+    }
+}
