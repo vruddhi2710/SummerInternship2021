c93b0379074499a9bc1b2e400f70ad971c154385
vruddhi2710 <59500931+vruddhi2710@users.noreply.github.com>
2021-05-27 23:31:05 +0530

    Add files via upload

---
+++ Task2/In-sem1/In-sem1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace In_sem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task2/In-sem1/Question1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            int sum = Calculator.AddNums(a, b);
+            /* fill in the blank below*/
+            string output = "The sum of "+a+" and "+b+" is "+sum;
+                Console.WriteLine(output);
+        }
+    }
+    class Calculator
+    {
+        public static int AddNums(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
