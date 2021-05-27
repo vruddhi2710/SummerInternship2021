18e906519caf4f093b99386d7cc659ccaafb3ffe
vruddhi2710 <59500931+vruddhi2710@users.noreply.github.com>
2021-05-27 23:34:22 +0530

    Add files via upload

---
+++ Task2/In-sem1/Question5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void AnArray()
+            {
+                /*your answer goes here*/
+                object[] TestArray = new object[4];
+                TestArray[0] = 10;
+                TestArray[1] = false;
+                TestArray[2] = new DateTime(2019, 01, 01);
+                TestArray[3] = "sample string";
+
+                foreach (object obj in TestArray)
+                {
+                    Console.WriteLine(obj);
+                    
+                }
+                //Console.ReadLine();
+            }
+            AnArray();
+        }
+    }
+}
---
+++ Task2/In-sem1/Question6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class Point
+        {
+            public static int X
+            {
+                get
+                {
+                    return X;
+                }
+            }
+            public static int Y
+            {
+                get
+                {
+                    return Y;
+                }
+            }
+            public Point(int x,int y)
+            {
+                x = X;
+                y = Y;
+            }
+            public static Tuple<int,int> Deconstruct()
+            {
+                return Tuple.Create(X, Y);
+            }
+        }
+    }
+}
+
